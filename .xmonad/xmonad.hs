886d0847bca1bf033400df26447d89a46448d890
Tsubonuma Kunio <kunio.tbnm@gmail.com>
2015-12-26 19:33:24 +0900

    xmonad.hs修正

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,9 +2,15 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
 
+myTerminal = "urxvt"
+
+myModMask = mod1Mask
+
+myLayoutHook = smartBorders $ layoutHook defaultConfig
+
 main = do
     xmonad =<< xmobar defaultConfig
-        { terminal = "urxvt"
-        , modMask = mod1Mask
-        , layoutHook = smartBorders $ layoutHook defaultConfig
+        { terminal = myTerminal
+        , modMask = myModMask
+        , layoutHook = myLayoutHook
         }
