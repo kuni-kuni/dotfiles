9ab6bb16934936a732783e42c6a3ae600db5b190
Tsubonuma Kunio <kunio.tbnm@gmail.com>
2015-12-26 19:28:43 +0900

    StdinReaderを使えるようにした

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,27 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
+import XMonad.Util.Run
+
+myTerminal = "urxvt"
+
+myModMask = mod1Mask
+
+myLayoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
+
+myLogHook h = dynamicLogWithPP $ defaultPP
+    { ppOutput = hPutStrLn h
+    }
+
+myManageHook = manageDocks <+> manageHook defaultConfig
 
 main = do
-    xmonad =<< xmobar defaultConfig
-        { terminal = "urxvt"
-        , modMask = mod1Mask
-        , layoutHook = smartBorders $ layoutHook defaultConfig
+    xmproc <- spawnPipe "xmobar"
+    xmonad defaultConfig
+        { terminal = myTerminal
+        , modMask = myModMask
+        , layoutHook = myLayoutHook
+        , logHook = myLogHook xmproc
+        , manageHook = myManageHook
         }
