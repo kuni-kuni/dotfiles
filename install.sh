1b98002b03efa5482366d39a3bb8162eb1d343ee
Tsubonuma Kunio <kunio.tbnm@gmail.com>
2015-12-26 13:36:31 +0900

    commit

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
+ln -sf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 
 mkdir -p ~/.xmonad
 ln -sf ~/dotfiles/xmonad.hs ~/.xmonad/xmohad.hs
