fef726109d7c22cdaa4b007d1d2e92fab0b90b71
Tsubonuma Kunio <kunio.tbnm@gmail.com>
2015-12-28 09:24:05 +0900

    inputrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 ln -sf ~/dotfiles/.vimrc ~/
 ln -sf ~/dotfiles/.Xresources ~/
 ln -sf ~/dotfiles/.vimperatorrc ~/
+ln -sh ~/dotfiles/.inputrc ~/
 
 mkdir -p ~/.xmonad
 ln -sf ~/dotfiles/.xmonad/xmonad.hs ~/.xmonad/
