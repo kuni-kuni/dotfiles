dfc4f321d10c62254d7e7ddd6982fe1ab9323d83
kunio <kunio.tbnm@gmail.com>
2016-05-03 19:20:04 +0900

    ubutnu_first

---
+++ ubuntu_first.sh
@@ -0,0 +1,11 @@
+# sudo apt-get update
+# sudo apt-get update
+# sudo apt-get upgrade
+# sudo apt-get install -y git
+# cd ~/
+# git clone https://github.com/kuni-kuni/dotfiles
+./dotfiles/install.sh
+sudo apt-get install -y vim
+sudo apt-get install -y haskell-platform
+sudo apt-get install -y xmonad libghc-xmonad-contrib-dev xmobar
+sudo apt-get install -y ibus-mozc
