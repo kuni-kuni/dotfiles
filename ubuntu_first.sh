bda035d444d59e3ad620c1405a79dcc23c0ce6d3
kunio <kunio.tbnm@gmail.com>
2016-05-04 10:06:30 +0900

    commit

--- ubuntu_first.sh
+++ ubuntu_first.sh
@@ -5,7 +5,7 @@
 # cd ~/
 # git clone https://github.com/kuni-kuni/dotfiles
 ./dotfiles/install.sh
-sudo apt-get install -y vim
+sudo apt-get install -y vim-gnome
 sudo apt-get install -y haskell-platform
 sudo apt-get install -y xmonad libghc-xmonad-contrib-dev xmobar
 sudo apt-get install -y rxvt-unicode-256color
